ca0979229ef00c3f1c8522344f705441dc2564b9
ismailyilmazer <ismail123yilmazer@gmail.com>
2023-07-07 10:55:14 +0300

    Add project files.

---
+++ src/Acme.BookStore.Application/BookAppService.cs
@@ -0,0 +1,31 @@
+﻿using Acme.BookStore.Permissions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Books;
+
+public class BookAppService_1 :
+      CrudAppService<
+        Book, //The Book entity
+        BookDto, //Used to show books
+        Guid, //Primary key of the book entity
+        PagedAndSortedResultRequestDto, //Used for paging/sorting
+        CreateUpdateBookDto>, //Used to create/update a book
+    IBookAppService //implement the IBookAppService
+{
+    public BookAppService_1(IRepository<Book, Guid> repository)
+        : base(repository)
+    {
+
+    }
+
+    public Task<ListResultDto<AuthorLookupDto>> GetAuthorLookupAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
